c2092c78cfce3334757100462a107e12f409f5d9
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2023-11-23 08:56:05 +1300

    Add dynamic unloading support

--- custom_components/managemyhealth/__init__.py
+++ custom_components/managemyhealth/__init__.py
@@ -5,37 +5,21 @@ from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
 from .api import MMHApi
-
-from .const import (
-    DOMAIN,
-    SENSOR_NAME,
-    PLATFORMS
-)
+from .const import DOMAIN, SENSOR_NAME, PLATFORMS
 
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry
-) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up ManageMyHealth from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry
-) -> bool:
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
     return unload_ok
