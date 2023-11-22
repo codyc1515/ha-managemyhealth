cf892b285ca5ab30aab864c88b023a7073329a24
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2023-11-22 15:35:48 +1300

    Add unloading support

--- custom_components/managemyhealth/__init__.py
+++ custom_components/managemyhealth/__init__.py
@@ -8,7 +8,8 @@ from .api import MMHApi
 
 from .const import (
     DOMAIN,
-    SENSOR_NAME
+    SENSOR_NAME,
+    PLATFORMS
 )
 
 
@@ -18,11 +19,23 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry
-):
+) -> bool:
+    """Set up ManageMyHealth from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setup(entry, PLATFORMS)
     )
     return True
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
--- custom_components/managemyhealth/const.py
+++ custom_components/managemyhealth/const.py
@@ -1,7 +1,12 @@
 """Constants for the ManageMyHealth sensors"""
+from homeassistant.const import Platform
 
 DOMAIN = 'managemyhealth'
 SENSOR_NAME = "ManageMyHealth"
 
 SENSOR_NAME_APPOINTMENT = 'Appointment'
 SENSOR_NAME_MAILBOX = 'Message'
+
+PLATFORMS = [
+    Platform.SENSOR,
+]
